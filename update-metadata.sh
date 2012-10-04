feb664e2659b7913273611d5f0e88fdded88c22f
Chris Fordham <chris.fordham@rightscale.com>
2012-10-04 13:47:38 +1000

    Add removal of collectd* cookbook .gitignore files.

--- update-metadata.sh
+++ update-metadata.sh
@@ -3,8 +3,8 @@
 # generate metadata.json for upstream opscode cookbooks that have no metadata.json
 knife cookbook metadata apache2 -o cookbooks/
 knife cookbook metadata build-essential -o cookbooks/
-knife cookbook metadata collectd -o cookbooks/
-knife cookbook metadata collectd_plugins -o cookbooks/
+knife cookbook metadata collectd -o cookbooks/; rm ./cookbooks/collectd/.gitignore
+knife cookbook metadata collectd_plugins -o cookbooks/; rm ./cookbooks/collectd_plugins/.gitignore
 knife cookbook metadata cron -o cookbooks/
 knife cookbook metadata freeswitch -o cookbooks/
 knife cookbook metadata ntp -o cookbooks/
