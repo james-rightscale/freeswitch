efa487147eda331d66cb47cace1e43dbb8a26381
Chris Fordham <chris.fordham@rightscale.com>
2012-10-04 13:26:17 +1000

    Update Cheffile to use flaccid's fork of the freeswitch cookbook; update cookbooks via librarian; refresh metadata.json files.

--- cookbooks/freeswitch/metadata.rb
+++ cookbooks/freeswitch/metadata.rb
@@ -1,6 +1,9 @@
-maintainer       "Example Com"
-maintainer_email "ops@example.com"
+maintainer       "Chris Fordham"
+maintainer_email "chris.fordham@rightscale.com"
 license          "Apache 2.0"
 description      "Installs/Configures freeswitch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
+
+recipe "freeswitch", "Installs/configures FreeSWITCH."
+recipe "freeswitch::security", "Installs OS level security requirements for FreeSWITCH."
